bfae8a0781e15d74c9c56ecfffce8807f711a757
Vishal jain <vishaljain7385@gmail.com>
2025-08-03 15:25:07 +0530

    load the initia solution

---
+++ ExtendedProperties.sql
@@ -0,0 +1,6 @@
+﻿EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'AdventureWorks 2016 Sample OLTP Database';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = N'Primary filegroup for the AdventureWorks 2016 sample database.', @level0type = N'FILEGROUP', @level0name = N'PRIMARY';
+
